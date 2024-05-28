3ef82ab403a0316d735c603e0b5abe07b4f37585
silviaamorim <silvia.nobaya@gmail.com>
2024-05-28 15:19:33 -0300

    PROJETO V1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 let nome = "silvia";
-console.log (nome);
+let nivel = 1025;
+ switch (nivel)
+ {
+    case (nivel<=1000):
+        console.log ("Ferro");
+        break;
+    case (nivel>1000) && (nive<=2000):
+        console.log ("Bronze");
+        break;
+    case (nivel>=2001) && (nivel<=5000):
+        console.log ("Prata");
+        break;
+    case (nivel>=5001) && (nivel<=7000):
+        console.log ("Ouro");
+        break;
+    case (nivel>=7001) && (nivel<=8000):
+        console.log ("Platina");
+        break;
+    case (nivel>=8001) && (nivel<=9000):
+        console.log ("Ascendente");
+        break;
+    case (nivel>=9001) && (nivel<=10000):
+        console.log ("Imortal");
+        break;
+    case (nivel>=10001):
+            console.log ("Radiante");
+            break;
+    console.log ("O Herói de nome " (nome) + "está no nível de " (nivel));
+    default: ("Favor inserir apenas numeros");
+}
